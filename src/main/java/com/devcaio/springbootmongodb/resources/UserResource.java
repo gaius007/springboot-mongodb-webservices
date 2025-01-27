e0233739c9794492dd6d84c16303c31787b95a20
gaius007 <c.oliveira2005@outlook.com>
2025-01-27 13:57:05 -0300

    User insertion with POST

--- src/main/java/com/devcaio/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/devcaio/springbootmongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.devcaio.springbootmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,12 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.devcaio.springbootmongodb.domain.User;
 import com.devcaio.springbootmongodb.dto.UserDTO;
 import com.devcaio.springbootmongodb.services.UserService;
-import com.devcaio.springbootmongodb.services.exception.ObjectNotFoundException;
 
 @RestController
 @RequestMapping(value="/users")
@@ -30,6 +34,14 @@ public class UserResource {
 	@GetMapping(value="/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {	
 		return ResponseEntity.ok().body(new UserDTO(service.findById(id)));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+		User user = service.fromDTO(userDTO);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/com/devcaio/springbootmongodb/services/UserService.java
+++ src/main/java/com/devcaio/springbootmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devcaio.springbootmongodb.domain.User;
+import com.devcaio.springbootmongodb.dto.UserDTO;
 import com.devcaio.springbootmongodb.repository.UserRepository;
 import com.devcaio.springbootmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 	
 }
