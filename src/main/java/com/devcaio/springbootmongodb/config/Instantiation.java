8db7f1bc96f58587a3b6cd513d309d17e04e95fe
gaius007 <c.oliveira2005@outlook.com>
2025-01-27 20:26:21 -0300

    Creating Post entity with nested User

--- src/main/java/com/devcaio/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/devcaio/springbootmongodb/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.devcaio.springbootmongodb.config;
 
+import java.time.Instant;
 import java.util.Arrays;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.devcaio.springbootmongodb.domain.Post;
 import com.devcaio.springbootmongodb.domain.User;
+import com.devcaio.springbootmongodb.repository.PostRepository;
 import com.devcaio.springbootmongodb.repository.UserRepository;
 
 @Configuration
@@ -14,6 +18,9 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -26,6 +33,11 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
+		Post post1 = new Post(null, Date.from(Instant.now()), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, Date.from(Instant.now()), "Dicas", "Não se aprende escutando e sim fazendo! #foco", alex);
+		Post post3 = new Post(null, Date.from(Instant.now()), "How to learn a new language?", "Can someone explain how can i increase my english skills?", bob);
+		
+		postRepository.saveAll(Arrays.asList(post1, post2, post3));
 	}
 	
 }
---
+++ src/main/java/com/devcaio/springbootmongodb/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.devcaio.springbootmongodb.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devcaio.springbootmongodb.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
