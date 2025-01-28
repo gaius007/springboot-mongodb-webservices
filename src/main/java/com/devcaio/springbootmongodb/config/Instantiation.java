80cacf945548aec7c2dd2399d9a420cd3939ded8
gaius007 <c.oliveira2005@outlook.com>
2025-01-27 21:26:13 -0300

    Author data projection with DTO

--- src/main/java/com/devcaio/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/devcaio/springbootmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.devcaio.springbootmongodb.domain.Post;
 import com.devcaio.springbootmongodb.domain.User;
+import com.devcaio.springbootmongodb.dto.AuthorDTO;
 import com.devcaio.springbootmongodb.repository.PostRepository;
 import com.devcaio.springbootmongodb.repository.UserRepository;
 
@@ -33,9 +34,9 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, Date.from(Instant.now()), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, Date.from(Instant.now()), "Dicas", "Não se aprende escutando e sim fazendo! #foco", alex);
-		Post post3 = new Post(null, Date.from(Instant.now()), "How to learn a new language?", "Can someone explain how can i increase my english skills?", bob);
+		Post post1 = new Post(null, Date.from(Instant.now()), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, Date.from(Instant.now()), "Dicas", "Não se aprende escutando e sim fazendo! #foco", new AuthorDTO(alex));
+		Post post3 = new Post(null, Date.from(Instant.now()), "How to learn a new language?", "Can someone explain how can i increase my english skills?", new AuthorDTO(bob));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2, post3));
 	}
